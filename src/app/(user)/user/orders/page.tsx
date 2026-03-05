3ae1eed9d0bc47d341c5d680859b62795b2e7f10
karamo10 <pabimamie@gmail.com>
2026-03-05 12:17:01 +0000

    feat: add  development notice in Orders page

--- src/app/(user)/user/orders/page.tsx
+++ src/app/(user)/user/orders/page.tsx
@@ -1,17 +1,17 @@
 export default function Orders() {
   return (
-    <div className="bg-white rounded-xl shadow-xl mt-6">
+    <div className="bg-[#ffffff] border border-[#e2e8f0] rounded-xl mt-6">
       <div className="p-6 lg:p-8 bg-red-00">
         <div className="space-y-6">
           <div>
-            <h2 className="text-lg font-semibold">My Orders</h2>
+            <h2 className="text-sm font-semibold">My Orders</h2>
           </div>
           <div className="space-y-6">
             <div>
               <div className="text-center py-10">
-                <p className="text-clr-secondary">No orders found</p>
+                <p className="text-clr-secondary capitalize">⚠️ under development </p>
                 <p className="text-xs text-gray-400 mt-2">
-                  Start shopping to see your orders here.
+                 No orders found
                 </p>
               </div>
             </div>
