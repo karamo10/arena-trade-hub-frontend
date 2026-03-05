d8de89f77cea521c91409ccd3aa555abe7574217
karamo10 <pabimamie@gmail.com>
2026-03-05 12:15:36 +0000

    feat: add Cart component with development notice

---
+++ src/app/(main)/cart/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/ui/layout/Container';
+export default function Cart() {
+  return (
+    <section>
+      <Container>
+        <div className="text-center py-20">
+          <p className="text-clr-secondary capitalize">
+            ⚠️ <br /> Cart page is under development.
+          </p>
+        </div>
+      </Container>
+    </section>
+  );
+}
