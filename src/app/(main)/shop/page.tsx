3560ec1d5fbd16a3c44dd5e5c20906ef4ee5de12
karamo10 <pabimamie@gmail.com>
2026-03-05 12:16:22 +0000

    feat: add Shop page with development notice

---
+++ src/app/(main)/shop/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/ui/layout/Container';
+export default function Shop() {
+  return (
+    <section>
+      <Container>
+        <div className="text-center py-20">
+          <p className="text-clr-secondary capitalize">
+            ⚠️ <br /> This page is under development.
+          </p>
+        </div>
+      </Container>
+    </section>
+  );
+}
