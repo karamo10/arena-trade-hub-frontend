bce39ad0dd6c2587c8d6388560dbdea2d96be5ea
karamo10 <pabimamie@gmail.com>
2026-03-05 12:15:02 +0000

    feat: add background color to MainLayout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-[#fffffff]">
           <Header />
           <section className='flex-1'>
           {children}
